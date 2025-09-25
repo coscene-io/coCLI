2ece77bc831fd96f3baa59799dcfb0af543f8818
Yujing Zheng <yujing@coscene.io>
2025-09-25 14:07:47 +0800

    update ignore

--- make/cocli/all.mk
+++ make/cocli/all.mk
@@ -1,5 +1,5 @@
 FILE_IGNORES := $(FILE_IGNORES) \
-	.vscode/ .DS_Store .idea/ bin/
+	.vscode/ .DS_Store .idea/ bin/ .cache/
 
 GO_ALL_REPO_PKGS := ./cmd/... ./internal/... ./pkg/... ./api/...
 GO_BINS := $(GO_BINS) \
