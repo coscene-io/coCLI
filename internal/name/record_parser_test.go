473adbdb3960ecbeeae151a86915962c15d2a9f1
Yujing Zheng <yujingz@users.noreply.github.com>
2026-05-05 19:27:59 +0800

    fix record resource name parsing (#167)

---
+++ internal/name/record_parser_test.go
@@ -0,0 +1,33 @@
+// Copyright 2026 coScene
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package name
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewRecordRejectsNestedResourcePath(t *testing.T) {
+	_, err := NewRecord("projects/p1/records/r1/files/data.bin")
+
+	require.Error(t, err)
+}
+
+func TestNewRecordRejectsProjectIDWithSlash(t *testing.T) {
+	_, err := NewRecord("projects/p1/records/r1/records/r2")
+
+	require.Error(t, err)
+}
